96c34f72cfa56b58657031ca8f91a963a0a74e62
Tushar Kathuria <tushar.kathuria@compass.com>
2021-03-16 18:11:48 +0530

    Added dotenv

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+require('dotenv').config()
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log('Containers rule!');
+    console.log(`Containers rule! Pipeline: ${process.env.PIPELINE}`);
     await sleep(5000);
   }
 }
